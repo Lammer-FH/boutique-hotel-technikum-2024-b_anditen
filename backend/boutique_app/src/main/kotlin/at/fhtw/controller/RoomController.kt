2c7725653740d746f5c6479306d445dbdd9f538d
Thomas <thomas@perondas.dev>
2024-06-02 12:08:14 +0200

    chore: Update RoomController with isRoomAvailable endpoint

--- backend/boutique_app/src/main/kotlin/at/fhtw/controller/RoomController.kt
+++ backend/boutique_app/src/main/kotlin/at/fhtw/controller/RoomController.kt
@@ -2,10 +2,14 @@ package at.fhtw.controller
 
 import at.fhtw.dtos.responses.Room
 import at.fhtw.model.repositories.RoomRepository
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.sql.Date
+import java.time.LocalDate
 
 @RequestMapping("/rooms")
 @RestController
@@ -16,4 +20,12 @@ class RoomController(val roomRepository: RoomRepository) {
 
     @GetMapping("/{id}")
     fun getRoomById(@PathVariable id: Long): Room = roomRepository.findById(id).map { Room.from(it) }.orElseThrow()
+
+    @GetMapping("/{id}/available")
+    fun isRoomAvailable(
+        @PathVariable id: Long,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) start: LocalDate,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) end: LocalDate
+    ): Boolean =
+        roomRepository.checkIfRoomIsAvailable(id, Date.valueOf(start), Date.valueOf(end))
 }
