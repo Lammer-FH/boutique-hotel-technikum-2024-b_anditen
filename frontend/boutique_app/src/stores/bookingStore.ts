8ee24dbb4a42b1bcb6ff7a37ecb5eebd4020cf18
Dominik Rauch <dominikrauch@gmx.net>
2024-06-16 21:12:42 +0200

    Added bookingStore

---
+++ frontend/boutique_app/src/stores/bookingStore.ts
@@ -0,0 +1,39 @@
+import {defineStore} from 'pinia';
+import axios from 'axios';
+import Customer from '@/models/customer';
+
+export const useBookingStore = defineStore('booking', {
+    state: () => ({
+        customer: new Customer('', '', '', '', '', ''),
+        numberOfGuests: 1,
+        breakfast: false,
+        roomId: 0,
+        loading: false,
+    }),
+    actions: {
+        async bookRoom(customer: Customer, roomId: number, numberOfGuests: number, breakfast: boolean, startDate: string, endDate: string) {
+            if (!customer) {
+                throw new Error('Customer not set');
+            }
+            this.customer = customer;
+            this.roomId = roomId;
+
+            return await axios.post(import.meta.env.VITE_API_BASE_URL + '/bookings', {
+                roomIds: [roomId],
+                startDate,
+                endDate,
+                customer: {
+                    firstName: customer.firstName,
+                    lastName: customer.lastName,
+                    email: customer.email,
+                    phoneNumber: customer.phoneNumber,
+                    birthDate: customer.birthDate,
+                },
+                numberOfGuests: numberOfGuests,
+                breakfast: breakfast,
+            }, {
+                validateStatus: status => (status >= 200 && status < 499)
+            });
+        },
+    },
+});
